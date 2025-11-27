f04d7aa36d3b79e6bbcae47646edda111140f289
“winhuor007” <winhuorhan@gmail.com>
2025-11-27 11:20:36 +0700

    hehhehe

--- src/components/ScalarDoc.tsx
+++ src/components/ScalarDoc.tsx
@@ -4,8 +4,18 @@ import { ApiReferenceReact } from '@scalar/api-reference-react';
 
 export default function ScalarDoc() {
   return (
-    <div >
-      <ApiReferenceReact configuration={{ url: `${import.meta.env.BASE_URL}openapi.yaml`, layout: 'modern', hideModels: true, hideClientButton: true,showDeveloperTools: "never", }} />
+    <div style={{ height: "100vh", width: "100%", overflow: "hidden" }}>
+      <ApiReferenceReact
+        configuration={{
+          spec: {
+            url: `${import.meta.env.BASE_URL}openapi.yaml`,
+          },
+          layout: 'modern',
+          hideModels: true,
+          hideSidebarToggle: true,
+          showDeveloperTools: "never"
+        }}
+      />
     </div>
   );
 }
